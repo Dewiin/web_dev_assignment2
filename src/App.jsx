aec9b2238d3db6763028f2d3cc21e5e051d31583
Devin Xie <devinxie418@gmail.com>
2025-04-22 17:00:34 -0400

    added background colors, and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [index, setIndex] = useState(0);
-  const colors = useRef([1,2,3,4,5]);
+  const colors = useRef(["#CC0003", "#FE6734", "#C4BC31", "#50723C", "#32C3BB", "#6852A3", "#7E3A5C", "#D88C9A"]);
+  const textColor = useRef(["white", "black", "black", "white", "black", "white", "white", "black"]);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = `${colors.current[index]}`;
+
+    const buttonText = document.querySelector(".change-color-button");
+    buttonText.style = `color: ${textColor.current[index]}`;
+  }, [index])
 
   function handleClick() {
     setIndex((prevState) => {
@@ -13,7 +21,7 @@ function App() {
 
   return (
     <>
-      <button className='change-color-button' onClick={handleClick}> {index} </button>
+      <button className='change-color-button' onClick={handleClick}> Click! </button>
     </>
   )
 }
